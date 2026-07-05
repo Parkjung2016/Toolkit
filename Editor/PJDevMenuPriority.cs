643b4ce38209696465a0a3368fd555713868e94b
Parkjung2016 <bagj21563@gmail.com>
2026-07-06 00:45:19 +0900

    [수정] MenuPriority 수정

--- Editor/PJDevMenuPriority.cs
+++ Editor/PJDevMenuPriority.cs
@@ -8,6 +8,7 @@ namespace PJDev.DevelopKit.Editors
         public const int Hub = MenuBarRoot;
         public const int Inventory = MenuBarRoot + 100;
         public const int GameplayTags = MenuBarRoot + 200;
+        public const int AnimMontage = MenuBarRoot + 250;
         public const int Ui = MenuBarRoot + 300;
         public const int CDebug = MenuBarRoot + 400;
         public const int Addressable = MenuBarRoot + 500;
